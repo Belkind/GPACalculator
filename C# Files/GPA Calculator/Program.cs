e71cfab231c82e50aac46d5f49390195a3557eba
Belkind <48163757+Belkind@users.noreply.github.com>
2019-03-03 13:04:11 -0500

    Add files via upload

---
+++ C# Files/GPA Calculator/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GPA Calculator:");
+            Console.ReadKey();
+            Console.WriteLine("This program will calculate your GPA for you.");
+            Console.ReadKey();
+            Console.WriteLine("Would you like a weighted or unweighted GPA?");
+
+            bool loop1 = true;
+            do
+            {
+                string answer = Console.ReadLine();
+                switch (answer.ToLower())
+                {
+                    case "unweighted":
+                        Unweighted();
+                        loop1 = false;
+                        break;
+
+                    case "weighted":
+                        Weighted();
+                        loop1 = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Please select a valid option.");
+                        break;
+                }
+            } while (loop1 == true);
+           
+        }
+
+        static void Unweighted()
+        {
+            Console.WriteLine("How many classes are you factoring into your GPA?");
+            int n = int.Parse(Console.ReadLine());
+
+            string[] classes = new string[n];
+            int[] grades = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("What is the name of this class?");
+                classes[i] = Console.ReadLine();
+                Console.WriteLine("What is your grade in this class?");
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+
+            float sum = grades.Sum();
+            float average = sum / n;
+            Console.WriteLine("Your average is " + average + "!");
+            Console.ReadKey();
+
+        }
+
+        static void Weighted()
+        {
+            Console.WriteLine("How many classes are you factoring into your GPA?");
+            int n = int.Parse(Console.ReadLine());
+
+            string[] classes = new string[n];
+            double[] grades = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("What is the name of this class?");
+                classes[i] = Console.ReadLine();
+
+                Console.WriteLine("Is this class normal, honors, or AP?");
+                bool loop2 = true;
+                do
+                {
+                    string classType = Console.ReadLine();
+                    switch (classType.ToLower())
+                    {
+                        case "normal":
+                            loop2 = false;
+                            Console.WriteLine("What is your grade in this class?");
+                            grades[i] = int.Parse(Console.ReadLine());
+                            break;
+
+                        case "honors":
+                            loop2 = false;
+                            Console.WriteLine("What is your grade in this class?");
+                            grades[i] = (double.Parse(Console.ReadLine())) * 1.05;
+                            break;
+
+                        case "ap":
+                            loop2 = false;
+                            Console.WriteLine("What is your grade in this class?");
+                            grades[i] = (double.Parse(Console.ReadLine())) * 1.10;
+                            break;
+
+                        default:
+                            Console.WriteLine("Please select a valid answer.");
+                            break;
+                    }
+                } while (loop2 == true);
+                
+            }
+
+            double sum = grades.Sum();
+            double average = sum / n;
+            Console.WriteLine("Your average is " + average + "!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Java Files/GPACalc/src/GPACalc.java
@@ -0,0 +1,192 @@
+import java.util.Scanner;
+import java.util.stream.*;
+
+public class GPACalc 
+{
+		static double honors = 1.05;
+		static double AP = 1.10;
+	
+		public static void main(String[] args)
+		{
+			Scanner APUSH = new Scanner(System.in);
+			
+			System.out.println("GPA Calculator: ");
+			
+			APUSH.nextLine();
+			System.out.println("This program will calculate your GPA for you.");
+			System.out.println("Would you like a weighted or unweighted GPA?");
+			
+			boolean calcType = true; 
+			do 
+			{
+				String answer = APUSH.nextLine();
+				switch (answer.toLowerCase())
+				{
+					case "unweighted":
+					Unweighted();
+					calcType = false;
+					break;
+					
+					case "weighted":
+					Weighted();
+					calcType = false;
+					break;
+					
+					default:
+					System.out.println("Please select a valid option!");
+					break;
+				}
+			} while (calcType == true);
+		}
+
+		static void Unweighted()
+		{
+			Scanner APUSH = new Scanner(System.in);
+			
+			System.out.println("How many classes are you factoring into your GPA?");
+			int n = Integer.parseInt(APUSH.nextLine());
+			
+			String[] classes = new String[n];
+			double[] grades = new double[n];
+			
+			for (int i = 0; i < n; i++)
+			{
+				System.out.println("What is the name of this class?");
+				classes[i] = APUSH.nextLine();
+				System.out.println("What is your grade in this class?");
+				grades[i] = Double.parseDouble(APUSH.nextLine());
+			}
+			
+			System.out.println();
+			
+			for (int i = 0; i < n; i++)
+			{
+				System.out.println(classes[i] + ": " + grades[i]);
+			}
+			
+			double sum =  DoubleStream.of(grades).sum();
+			double average = sum / n;
+			System.out.println("Your average is " + average);
+		}
+		
+		static void Weighted()
+		{
+			Scanner APUSH = new Scanner(System.in);
+			
+			System.out.println("How many classes are you factoring into your gpa?");
+			
+			int n = Integer.parseInt(APUSH.nextLine());
+			
+			String[] classes = new String[n];
+			double[] grades = new double[n];
+			
+			setCustomScales();
+			
+			for (int i = 0; i < n; i++)
+			{
+				System.out.println("What is the name of this class?");
+				classes[i] = APUSH.nextLine();
+				
+				System.out.println("Is this class normal, honors, or AP?");
+				boolean classType = true;
+				do 
+				{
+					String answer = APUSH.nextLine();
+					switch (answer.toLowerCase())
+					{
+						case "normal":
+						classType = false;
+						System.out.println("What is your grade in this class?");
+						grades[i] = Double.parseDouble(APUSH.nextLine());
+						break;
+						
+						case "honors":
+						classType = false;
+						System.out.println("What is your grade in this class?");
+						grades[i] = (Double.parseDouble(APUSH.nextLine())) * honors;
+						break;
+						
+						case "ap":
+						classType = false;
+						System.out.println("What is your grade in this class?");
+						grades[i] = (Double.parseDouble(APUSH.nextLine())) * AP;
+						break;
+						
+						default:
+						System.out.println("Please select a valid answer.");
+						break;
+						
+					}
+				} while (classType == true);
+			}
+			
+			for (int i = 0; i < n; i++)
+			{
+				System.out.println(classes[i] + ": " + grades[i]);
+			}
+			
+			double sum =  DoubleStream.of(grades).sum();
+			double average = sum / n;
+			System.out.println("Your average is " + average);
+		}
+		
+		static void setCustomScales()
+		{
+			Scanner APUSH = new Scanner(System.in);
+			
+			System.out.println("Do you want to use default or custom values for scaling your classes by type?");
+			
+			boolean custom = false;
+			
+			boolean askcustomLoop = true;
+			do
+			{
+				String askcustom = APUSH.nextLine();
+				switch (askcustom)
+				{
+					case "default":
+					askcustomLoop = false;
+					break;
+				
+					case "custom":
+					askcustomLoop = false;
+					custom = true;
+					break;
+						
+					default:
+					System.out.println("Sorry, can you please answer with the words default or custom?");
+					break;
+				}
+			} while (askcustomLoop == true);
+			
+			if (custom = true) {
+				System.out.println("What scale should be used for honors classes?");
+				honors = Double.parseDouble(APUSH.nextLine());
+				System.out.println("What scale should be used for AP classes?");
+				AP = Double.parseDouble(APUSH.nextLine());
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+}
